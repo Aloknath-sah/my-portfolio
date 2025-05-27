df772ba0b98b2e92f29a355adfe5a8c0a73db27b
Aloknath-sah <kumaraditya6396@gmail.com>
2025-05-27 15:52:03 +0530

    updated code

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -16,7 +16,7 @@ export const Projects = () => {
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               <h3 className="text-xl font-bold mb-2"> Chat Buddy</h3>
               <p className="text-gray-400 mb-4">
-                Chat Buddy is a chat webapp is a real-time chat application that allows
+                Chat Buddy is a real-time chat application that allows
                 users to communicate with each other instantly. It uses
                 WebSockets to enable bi-directional communication and supports
                 multiple users simultaneously.
